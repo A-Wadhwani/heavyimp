d743a20a4ac9908492751376728ecb4420a52a56
A-Wadhwani <wadhwani@purdue.edu>
2022-11-23 16:02:00 -0500

    Statement Evaluation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -28,6 +28,7 @@ fn eval_expr(expr: &Expr, store: &Sigma, heap: &Heap) -> EvalResult<Constant> {
         }
         // Return the constant
         Expr::Constant(c) => Ok(c.clone()),
+        // Evaluate expressions if they're the correct values
         Expr::NatAdd(a, b) => {
             let a = eval_expr(a, store, heap)?;
             let b = eval_expr(b, store, heap)?;
@@ -59,5 +60,68 @@ fn eval_expr(expr: &Expr, store: &Sigma, heap: &Heap) -> EvalResult<Constant> {
                 _ => Err(TypeMismatch),
             }
         }
+    }
+}
+
+fn eval_stmnt(stmnt: &Statement, store: &mut Sigma, heap: &mut Heap) -> EvalResult<()> {
+    match stmnt {
+        Statement::StoreAssign(id, expr) => {
+            let value = eval_expr(expr, store, heap)?;
+            store.insert(id.clone(), Value::Constant(value));
+            Ok(())
+        }
+        Statement::HeapNew(id, expr) => {
+            let value = eval_expr(expr, store, heap)?;
+            let index = heap.len();
+            heap.push(value);
+            store.insert(id.clone(), Value::Location(index));
+            Ok(())
+        }
+        Statement::HeapUpdate(id, expr) => {
+            let value = eval_expr(expr, store, heap)?;
+            let index = *store.get(id).ok_or(UnboundVariable).and_then(|v| match v {
+                Value::Constant(_) => Err(TypeMismatch),
+                Value::Location(l) => Ok(l),
+            })?;
+            // Check if the index is in the heap, and if it is, update it
+            heap.get_mut(index)
+                .ok_or(InvalidDereference)
+                .map(|c| *c = value)
+        }
+        Statement::HeapAlias(new_id, curr_id) => {
+            let index = *store
+                .get(curr_id)
+                .ok_or(UnboundVariable)
+                .and_then(|v| match v {
+                    Value::Constant(_) => Err(TypeMismatch),
+                    Value::Location(l) => Ok(l),
+                })?;
+            store.insert(new_id.clone(), Value::Location(index));
+            Ok(())
+        }
+        Statement::Sequence(s1, s2) => {
+            eval_stmnt(s1, store, heap)?;
+            eval_stmnt(s2, store, heap)
+        }
+        Statement::Conditional(expr, then_s, else_s) => {
+            let value = eval_expr(expr, store, heap)?;
+            match value {
+                Bool(true) => eval_stmnt(then_s, store, heap),
+                Bool(false) => eval_stmnt(else_s, store, heap),
+                _ => Err(TypeMismatch),
+            }
+        }
+        Statement::While(expr, loop_s) => {
+            let value = eval_expr(expr, store, heap)?;
+            match value {
+                Bool(true) => {
+                    eval_stmnt(loop_s, store, heap)?;
+                    eval_stmnt(stmnt, store, heap)
+                }
+                Bool(false) => Ok(()),
+                _ => Err(TypeMismatch),
+            }
+        }
+        Statement::Skip => Ok(()),
     }
 }
