2609f0f1073010ec881b115e04c8ac878fc1baf9
Mikail Khan <mikail@mikail-khan.com>
2022-11-24 18:50:00 -0500

    typechecker fixes

--- src/typechecker.rs
+++ src/typechecker.rs
@@ -10,6 +10,11 @@ pub enum Type {
     Boolean,
 }
 
+pub fn typecheck(program: &Statement) -> Result<(), TypeError> {
+    let mut sigma = HashMap::new();
+    typecheck_stmt_aux(&mut sigma, program)
+}
+
 fn expect_ty(expected: Type, got: Type) -> Result<Type, TypeError> {
     if expected == got {
         Ok(expected)
@@ -31,7 +36,8 @@ fn typecheck_expr_aux(sigma: &HashMap<String, Type>, ast: &Expr) -> Result<Type,
         }
         Expr::HeapRead(x) => {
             let ix_ty = sigma.get(x).ok_or(TypeError::UnboundVariable)?;
-            expect_ty(Type::Location, *ix_ty)
+            expect_ty(Type::Location, *ix_ty)?;
+            Ok(Type::Number)
         }
         Expr::Constant(Constant::Nat(_)) => Ok(Type::Number),
         Expr::Constant(Constant::Bool(_)) => Ok(Type::Boolean),
@@ -41,14 +47,17 @@ fn typecheck_expr_aux(sigma: &HashMap<String, Type>, ast: &Expr) -> Result<Type,
         }
         Expr::NatLeq(a, b) => {
             expect_expr_ty(Type::Number, a, sigma)?;
-            expect_expr_ty(Type::Number, b, sigma)
+            expect_expr_ty(Type::Number, b, sigma)?;
+            Ok(Type::Boolean)
         }
         Expr::BoolAnd(a, b) => {
             expect_expr_ty(Type::Boolean, a, sigma)?;
-            expect_expr_ty(Type::Boolean, b, sigma)
+            expect_expr_ty(Type::Boolean, b, sigma)?;
+            Ok(Type::Boolean)
         }
         Expr::BoolNot(a) => {
-            expect_expr_ty(Type::Boolean, a, sigma)
+            expect_expr_ty(Type::Boolean, a, sigma)?;
+            Ok(Type::Boolean)
         }
     }
 }
@@ -57,27 +66,28 @@ fn typecheck_stmt_aux(sigma: &mut HashMap<String, Type>, ast: &Statement) -> Res
     match ast {
         Statement::StoreAssign(id, expr) => {
             let expr_ty = typecheck_expr_aux(sigma, expr)?;
-            expect_ty(Type::Number, expr_ty).and_then(|ty| {
-                sigma.insert(id.clone(), ty);
+            expect_ty(Type::Number, expr_ty).and_then(|_| {
+                sigma.insert(id.clone(), Type::Location);
                 Ok(())
             })
         }
         Statement::HeapNew(id, expr) => {
             let expr_ty = typecheck_expr_aux(sigma, expr)?;
-            expect_ty(Type::Location, expr_ty).and_then(|ty| {
-                sigma.insert(id.clone(), ty);
+            expect_ty(Type::Number, expr_ty).and_then(|_| {
+                sigma.insert(id.clone(), Type::Location);
                 Ok(())
             })
         }
         Statement::HeapUpdate(id, expr) => {
             let expr_ty = typecheck_expr_aux(sigma, expr)?;
-            let stored_ty = sigma.get(id).ok_or(TypeError::UnboundVariable)?;
-            expect_ty(*stored_ty, expr_ty).map(|_| ())
+            let id_ty = sigma.get(id).ok_or(TypeError::UnboundVariable)?;
+            expect_ty(Type::Location, *id_ty).map(|_| ())?;
+            expect_ty(Type::Number, expr_ty).map(|_| ())
         }
         Statement::HeapAlias(alias, id) => {
             let stored_ty = sigma.get(id).ok_or(TypeError::UnboundVariable)?;
-            expect_ty(Type::Location, *stored_ty).and_then(|ty| {
-                sigma.insert(alias.clone(), ty);
+            expect_ty(Type::Location, *stored_ty).and_then(|_| {
+                sigma.insert(alias.clone(), Type::Location);
                 Ok(())
             })
         }
@@ -97,5 +107,39 @@ fn typecheck_stmt_aux(sigma: &mut HashMap<String, Type>, ast: &Statement) -> Res
         Statement::Skip => {
             Ok(())
         }
+    }
+}
+
+#[allow(unused)]
+mod test {
+    use crate::syntax::{*, Constant::*};
+    use super::*;
+
+    #[test]
+    fn basic_test() -> Result<(), TypeError> {
+        let program = Statement::Sequence(
+            Box::new(Statement::HeapNew("x".into(), Expr::Constant(Nat(1)))),
+            Box::new(Statement::Sequence(
+                    Box::new(Statement::Sequence(
+                            Box::new(Statement::HeapNew("z".into(), Expr::Constant(Nat(2)))),
+                            Box::new(Statement::HeapUpdate(
+                                    "z".into(),
+                                    Expr::NatAdd(
+                                        Box::new(Expr::HeapRead("x".into())),
+                                        Box::new(Expr::HeapRead("z".into())),
+                                    ),
+                            )),
+                    )),
+                    Box::new(Statement::Conditional(
+                            Expr::NatLeq(
+                                Box::new(Expr::HeapRead("x".into())),
+                                Box::new(Expr::Constant(Nat(0))),
+                            ),
+                            Box::new(Statement::HeapNew("y".into(), Expr::HeapRead("z".into()))),
+                            Box::new(Statement::HeapNew("y".into(), Expr::Constant(Nat(4)))),
+                    )),
+            )),
+            );
+        typecheck(&program)
     }
 }
