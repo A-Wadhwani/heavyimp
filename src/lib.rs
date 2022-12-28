ea1973a4310564d37b0fd4dc1e714d3c69caba5f
A-Wadhwani <wadhwani@purdue.edu>
2022-12-28 23:28:35 +0400

    Success Struct

--- src/lib.rs
+++ src/lib.rs
@@ -9,25 +9,32 @@ pub mod syntax;
 mod test;
 pub mod typechecker;
 
+#[wasm_bindgen(getter_with_clone)]
+pub struct Success {
+    pub parsed: String,
+    pub evaluated: String,
+}
+
 #[wasm_bindgen]
-pub fn run_str(source: &str) {
+pub fn run_program(source: &str) -> Result<Success, String> {
     let parsed = parser::parse(&source).unwrap_or_else(|e| {
         let ImpParseError::Other(s) = e;
         eprintln!("Parser Error:\n{}", s);
         std::process::exit(1);
     });
 
-    println!("Parsed");
-    println!("===============");
-    println!("{:?}", &parsed);
-
     let typecheck = typechecker::typecheck(&parsed);
     match typecheck {
         Ok(_) => {
-            println!("\nEvaluated");
-            println!("===============");
-            println!("{:?}", evaluator::eval_program(&parsed))
+            let evaluated = evaluator::eval_program(&parsed);
+            match evaluated {
+                Ok(e) => Ok(Success {
+                    parsed: format!("{:?}", parsed),
+                    evaluated: format!("{:?}", e),
+                }),
+                Err(e) => Err(format!("Evaluation Error: {:?}", e)),
+            }
         }
-        Err(e) => eprintln!("Error: {:?}", e),
+        Err(e) => Err(format!("Parsing Error: {:?}", e)),
     }
 }
